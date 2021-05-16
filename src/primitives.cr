e6a3367958af7d217abaa6817318899e2af36c4c
Groogy <groogy@groogy.se>
2021-05-16 13:31:59 +0200

    Added support to compare primitives with fixedpoint

--- src/primitives.cr
+++ src/primitives.cr
@@ -14,6 +14,10 @@ struct Float < Number
   def /(other : FixedPoint(U, S)) : FixedPoint forall U, S
     FixedPoint(U, S).new(self) / other
   end
+
+  def <=>(other : FixedPoint(U, S)) : Int32? forall U, S
+    FixedPoint(U, S).new(self) <=> other
+  end
 end
 
 struct Int < Number
@@ -31,5 +35,9 @@ struct Int < Number
 
   def /(other : FixedPoint(U, S)) : FixedPoint forall U, S
     FixedPoint(U, S).new(self) / other
+  end
+
+  def <=>(other : FixedPoint(U, S)) : Int32 forall U, S
+    FixedPoint(U, S).new(self) <=> other
   end
 end
